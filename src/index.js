0c57b405480e501f31dcd30952790f47c325b2f9
Charles Castillo <mrcodeislife718@proton.me>
2026-09-06 14:50:28 -0400

    Route Scout recovery through normalized public API

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 export { tokenize } from './tokenizer.js';
 export { tokenizeLossless, rawSlice, tokenAt } from './lossless.js';
 export { parse, parseValue } from './parser.js';
-export { parseTolerant, isRecoveryNode } from './recovery.js';
-export { parseRecovering } from './recovery-parser.js';
+export { parseTolerant, parseRecovering, isRecoveryNode } from './recovery.js';
 export { stringify, serializeDocument } from './serializer.js';
 export { toJSON, fromJSON } from './convert.js';
 export { getNode, getMember, setValue, createValueNode } from './document.js';
